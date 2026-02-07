105878bb14d49bb9a2a2669a9376e89ee2781a78
Francesco Bascelli <francescobascelli88@gmail.com>
2026-02-07 10:45:53 +0100

    7th commit
    Implemented clients input in the fetch

--- color-scheme-generator/src/main.js
+++ color-scheme-generator/src/main.js
@@ -11,8 +11,10 @@ btn.addEventListener('click', (event)=>{
 
     const chosenColor = color.value
     const chosenMode = option.value
+
+    const hex = chosenColor.slice(1)
    
-    fetch(`https://www.thecolorapi.com/scheme?hex=0047AB&format=html&mode=analogic&count=5`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&format=json&mode=${chosenMode}&count=5`)
     .then(res => res.json())
     .then(data => console.log(data))
     
