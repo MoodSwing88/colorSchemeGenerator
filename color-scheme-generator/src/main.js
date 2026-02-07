7d30e3e223798b3f496c21431ee1d074cfc147fd
Francesco Bascelli <francescobascelli88@gmail.com>
2026-02-07 10:41:38 +0100

    6th commit
    Adding fetch to color api

--- color-scheme-generator/src/main.js
+++ color-scheme-generator/src/main.js
@@ -11,6 +11,9 @@ btn.addEventListener('click', (event)=>{
 
     const chosenColor = color.value
     const chosenMode = option.value
-    console.log(chosenColor, chosenMode )
+   
+    fetch(`https://www.thecolorapi.com/scheme?hex=0047AB&format=html&mode=analogic&count=5`)
+    .then(res => res.json())
+    .then(data => console.log(data))
     
 })
