a5553f50448170197c3e2bba9a055d8b985ecc03
Francesco Bascelli <francescobascelli88@gmail.com>
2026-02-07 10:34:05 +0100

    5th Commit
    Made the JS get the client's input

--- color-scheme-generator/src/main.js
+++ color-scheme-generator/src/main.js
@@ -1,2 +1,16 @@
 import './style.css'
 
+const color = document.getElementById('color')
+const option = document.getElementById('select')
+const btn = document.getElementById('get-color')
+
+
+
+btn.addEventListener('click', (event)=>{
+    event.preventDefault();
+
+    const chosenColor = color.value
+    const chosenMode = option.value
+    console.log(chosenColor, chosenMode )
+    
+})
