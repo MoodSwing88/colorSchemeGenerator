63c9eb2bcca41048934679fe76a5a4d169478642
Francesco Bascelli <francescobascelli88@gmail.com>
2026-02-09 15:23:20 +0100

    Final commit
    The app show the colors and and the hex of the colors

--- color-scheme-generator/src/main.js
+++ color-scheme-generator/src/main.js
@@ -4,8 +4,23 @@ const color = document.getElementById('color')
 const option = document.getElementById('select')
 const btn = document.getElementById('get-color')
 
+const colorDivs = [
+  document.getElementById('color-one'),
+  document.getElementById('color-two'),
+  document.getElementById('color-three'),
+  document.getElementById('color-four'),
+  document.getElementById('color-five')
+]
 
-let colors
+const colorTexts = [
+  document.getElementById('show-one'),
+  document.getElementById('show-two'),
+  document.getElementById('show-three'),
+  document.getElementById('show-four'),
+  document.getElementById('show-five')
+]
+
+
 btn.addEventListener('click', (event)=>{
     event.preventDefault();
 
@@ -17,7 +32,16 @@ btn.addEventListener('click', (event)=>{
     fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&format=json&mode=${chosenMode}&count=5`)
     .then(res => res.json())
     .then(data => {
-        colors = data.colors.map(c => c.hex.value)
-        console.log(colors)
+        const colors = data.colors.map(c => c.hex.value)
+        
+        colorDivs.forEach((div, i)=>{
+            div.style.backgroundColor = colors[i]
+        })
+
+        colorTexts.forEach((p, i)=> {
+            p.textContent = colors[i]
+        })
+
     })
+
 })
